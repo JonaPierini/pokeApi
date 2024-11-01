9ca62ed4bb9b1014248b6da88d5e50054668f14c
JonaPierini <jonapierini@gmail.com>
2024-11-01 17:38:17 -0300

    fix:hashrouter-github-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { HashRouter } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { AppRouter } from "./presentacion/router/AppRouter";
 
 export const App = () => {
-  return (
-    <HashRouter>
-      <AppRouter />
-    </HashRouter>
-  );
+  const router = createHashRouter([
+    {
+      path: "/*",
+      element: <AppRouter />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
